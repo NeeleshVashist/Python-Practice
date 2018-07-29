5705421323d83982a5cce1e6f98374f0b2185039
nvkepler <neeleshvashist@gmail.com>
2018-07-29 21:35:54 +0530

    Add Que

---
+++ Extra Questions/MinMaxSplitReplace.py
@@ -0,0 +1,17 @@
+#Testing Functions
+	#Min - Max
+	#Replace
+	#Split
+
+a = int(input('Enter a : '))
+b = int(input('Enter b : '))
+c = int(input('Enter c : '))
+str = input('Enter a string : ')
+
+print('Minimum :',min(a,b,c))
+print('Maximum :',max(a,b,c))
+
+print('New String :',str.replace('World','Boy!'))
+
+string = 'android is awesome'
+print('Striped String :',string.strip('an'))
