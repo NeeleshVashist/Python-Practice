5bde3bd143a77d7300c30b86d4edd9f581395452
nvkepler <neeleshvashist@gmail.com>
2018-07-30 14:09:04 +0530

    Add Que

---
+++ Extra Questions/test.py
@@ -0,0 +1,10 @@
+#Just for Testing Purpose
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = []
+    for i in range(n):
+        a = int(input())
+        arr.append(a)
+
+    print(arr)
