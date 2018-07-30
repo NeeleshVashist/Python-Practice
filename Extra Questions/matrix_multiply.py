44b106f3735c4e37a5c57de7bd387c9d4891858d
nvkepler <neeleshvashist@gmail.com>
2018-07-30 14:08:12 +0530

    Add Que

---
+++ Extra Questions/matrix_multiply.py
@@ -0,0 +1,44 @@
+#2d Matrix Multiply
+
+list1 = []
+list2 = []
+list3 = []
+sub_list = []
+main_list = []
+
+z = 0
+
+m = int(input('Enter Number of Rows : '))
+n = int(input('Enter Number of columns : '))
+
+for i in range(0,m):
+    for j in range(0,n):
+        a = int(input('Enter a Number for first matrix: '))
+        sub_list.append(a)
+
+    list1.append(sub_list)
+    sub_list = []
+
+print('*************************************')
+
+for i in range(0,m):
+    for j in range(0,n):
+        a = int(input('Enter a Number for second matrix : '))
+        sub_list.append(a)
+
+    list2.append(sub_list)
+    sub_list = []
+
+for i in range(0,m):
+    for j in range(0,n):
+        for k in range(0,n):
+            z += list1[i][k] * list2[k][j]
+
+        sub_list.append(z)
+
+    list3.append(sub_list)
+    sub_list = []
+
+print('First List : ',list1)
+print('Second List : ',list2)
+print('Final List : ',list3)
