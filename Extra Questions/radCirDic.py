98007c2b5e5811afa650f2058e3169ab5129c663
nvkepler <neeleshvashist@gmail.com>
2018-07-30 14:07:10 +0530

    Add Que

---
+++ Extra Questions/radCirDic.py
@@ -0,0 +1,10 @@
+# Create a dictionary of radius in the range of 1 to 5 and corresponding to its
+# circumfrance
+
+first_dic = {}
+
+for i in range(1,6):
+
+    first_dic[i] = 2 * 3.14 * i
+
+print('Fibonacci Dictionary :',first_dic)
