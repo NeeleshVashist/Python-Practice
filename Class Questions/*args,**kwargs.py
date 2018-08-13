4cf74606399c2291b598043e90adc12ca9c58b42
nvkepler <neeleshvashist@gmail.com>
2018-08-13 22:25:29 +0530

    Add : *args,**kwargs Question

---
+++ Class Questions/*args,**kwargs.py
@@ -0,0 +1,8 @@
+def my_fun(**kwargs):
+    print(kwargs)
+
+stu1 = ['Naman',1610991550,'coder','Iska bhi nii pta']
+stu2 = ['Dhruv',1610991268,'coder','pta nii']
+stu3 = ['Neelesh',1610991563,'coder','bhot sare']
+
+my_fun(Student_1=stu1,Student_2=stu2,Student_3=stu3)
