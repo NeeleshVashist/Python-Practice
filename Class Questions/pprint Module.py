3ba68f73fae555fa74bbf3c92a23325c8fe625bd
nvkepler <neeleshvashist@gmail.com>
2018-08-14 10:07:12 +0530

    Add : pprint Module Question

---
+++ Class Questions/pprint Module.py
@@ -0,0 +1,8 @@
+# Task :
+#
+# Print the data in different styles using pprint module
+
+import pprint
+stuff = ['spam', 'eggs', 'lumberjack', 'knights', 'ni']
+stuff.insert(0, stuff)
+pprint.pprint(stuff)
