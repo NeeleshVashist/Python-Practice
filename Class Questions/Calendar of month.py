4879779bfbeee5c4880a2d884ae5329e2f2e0b3f
nvkepler <neeleshvashist@gmail.com>
2018-08-14 10:00:54 +0530

    Add : Calendar of month Question

--- Class Questions/Calendar of month.py
+++ Class Questions/Calendar of month.py
@@ -13,20 +13,9 @@ date = int(input('Enter Date : '))
 m = calendar.month(year,month,2,1)
 d = calendar.weekday(year,month,date)
 
-if(d == 0):
-    d = 'Monday'
-elif(d == 1):
-    d = 'Tuesday'
-elif(d == 2):
-    d = 'Wednesday'
-elif(d == 3):
-    d = 'Thusday'
-elif(d == 4):
-    d = 'Friday'
-elif(d == 5):
-    d = 'Saturday'
-elif(d == 6):
-    d = 'Sunday'
+now = datetime.datetime.now()
+day = now.strftime("%a")
 
 print(f'Calander of {month}th of Year {year} :\n',m)
-print(f'Weekday Number for {date} of {month}th of {year} : '+d)
+print(f'Weekday Number for {date} of {month}th of {year} : ',d)
+print(f'Day of {date} of {month}th of {year} : '+day)
