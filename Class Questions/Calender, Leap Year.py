bef16883a55bd5ad5bf42cf900c54470e8633eee
nvkepler <neeleshvashist@gmail.com>
2018-08-14 09:23:45 +0530

    Add : Calender, Leap Year Question

--- Class Questions/Calender, Leap Year.py
+++ Class Questions/Calender, Leap Year.py
@@ -5,6 +5,7 @@
 # From the current leap year to next leap year calculate the number of dates
 
 import calendar
+from datetime import date
 
 year = int(input('Enter Year : '))
 
@@ -12,10 +13,12 @@ cal = calendar.calendar(year,2,1,6)
 leap_year = calendar.isleap(year)
 
 if(leap_year == True):
-    total_days = (365 * 3) + (366 * 2)
+    d1 = date(year+5,1,1)
+    d2 = date(year,1,1)
+    total_days = d1-d2
     print('Calendar of year',year,' :\n',cal)
     print(year,' is a Leap Year\n')
-    print('Total No. of days from Current Leap Year to Next Leap Year : ',total_days)
+    print('Total No. of days from Current Leap Year to Next Leap Year : ',total_days.days)
 
 else:
     print('Calendar of year',year,' :\n',cal)
