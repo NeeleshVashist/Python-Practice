b30d4497513fc626b9430f34e9138bf50d83ec0b
nvkepler <neeleshvashist@gmail.com>
2018-08-14 13:50:55 +0530

    Add : Taxi Turn Question

--- Extra Questions/Taxi Turn.py
+++ Extra Questions/Taxi Turn.py
@@ -93,20 +93,26 @@ if __name__ == '__main__':
 
     test_case = int(input('Enter Nummber of Test Cases : '))
 
+    # Defining Lists, String(st), Variable(count)
     my_list = []
     new_list = []
     coordinate_list = []
     angle_list = []
-    side_list = []
+    modified_list = []
+    st = ''
+    count = 0
 
+    # Loop for Running Test Cases
     for i in range(test_case):
         no_of_coordinates = int(input('Enter No. of Coordinates : '))
 
+        # Checking Total No. of Coordinates entered
         while(no_of_coordinates < 3 or no_of_coordinates > 50):
             print('Minimum 3 and Maximum 50 Coordinates required')
             print('TRY AGAIN')
             no_of_coordinates = int(input('Enter No. of Coordinates : '))
 
+        # Loop for Entering Coordinates
         for j in range(no_of_coordinates):
             coordinates = input('Enter Coordinates(separated by a space) : ')
             coordinates = coordinates.split()
@@ -117,39 +123,62 @@ if __name__ == '__main__':
             my_list.append(new_list)
             new_list = []
 
-        diff_x = my_list[-2][0] - my_list[-3][0]
-        diff_y = my_list[-2][1] - my_list[-3][1]
+        COUNT = 1
 
-        X = my_list[-2][0] + diff_x
-        Y = my_list[-2][1] + diff_y
+        # Loop for Checking angle(sharp turns)
+        for k in range(COUNT,no_of_coordinates - 1):
 
-        coordinate_list.append(X)
-        coordinate_list.append(Y)
-        angle_list.append(coordinate_list)
-        angle_list.append(my_list[-2])
-        angle_list.append(my_list[-1])
+            # Breaking List into sub-lists
+            modified_list = my_list[k-1:k+2]
+            diff_x = modified_list[-2][0] - modified_list[-3][0]
+            diff_y = modified_list[-2][1] - modified_list[-3][1]
 
-        x1 = angle_list[0][0]
-        x2 = angle_list[1][0]
-        x3 = angle_list[2][0]
-        y1 = angle_list[0][1]
-        y2 = angle_list[1][1]
-        y3 = angle_list[2][1]
+            X = modified_list[-2][0] + diff_x
+            Y = modified_list[-2][1] + diff_y
 
-        #Finding Lengths of Sides
-        A = sqrt(((x1 - x2) ** 2) + ((y1 - y2) ** 2))
-        B = sqrt(((x2 - x3) ** 2) + ((y2 - y3) ** 2))
-        C = sqrt(((x3 - x1) ** 2) + ((y3 - y1) ** 2))
+            coordinate_list.append(X)
+            coordinate_list.append(Y)
+            angle_list.append(coordinate_list)
+            angle_list.append(modified_list[-2])
+            angle_list.append(modified_list[-1])
 
-        #Finding Angles
-        angle = degrees(acos(((A ** 2) + (B ** 2) - (C ** 2))/(2.0 * A * B)))
+            # Assigning Values to the Variables
+            x1 = angle_list[0][0]
+            x2 = angle_list[1][0]
+            x3 = angle_list[2][0]
+            y1 = angle_list[0][1]
+            y2 = angle_list[1][1]
+            y3 = angle_list[2][1]
 
-        # print(angle_list)
-        # print(angle)
-        coordinate_list = []
-        angle_list = []
+            #Finding Lengths of Sides
+            A = sqrt(((x1 - x2) ** 2) + ((y1 - y2) ** 2))
+            B = sqrt(((x2 - x3) ** 2) + ((y2 - y3) ** 2))
+            C = sqrt(((x3 - x1) ** 2) + ((y3 - y1) ** 2))
 
-        if(angle <= 45):
-            print('yes yes')
-        else:
-            print('no')
+            #Finding Angle
+            angle = degrees(acos(((A ** 2) + (B ** 2) - (C ** 2))/(2.0 * A * B)))
+
+            # Re-Assigning Lists
+            coordinate_list = []
+            angle_list = []
+            modified_list = []
+
+            # Checking Angles
+            if(angle <= 45):
+                flag = 1
+            else:
+                flag = 0
+                count += 1
+
+        if(flag == 1):
+            st += 'yes yes\n'
+        elif(count <= 2):
+            st += 'no yes\n'
+        elif(count > 2):
+            st += 'no no\n'
+
+        count = 0
+        my_list = []
+
+    print('\n**************** OUTPUT ******************')
+    print(st)
