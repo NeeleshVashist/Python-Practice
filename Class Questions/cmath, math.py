d84a1ce5129cb7b3fe22e4f0fd2412f60fe07c46
nvkepler <neeleshvashist@gmail.com>
2018-08-14 09:59:54 +0530

    Add : cmath, math Question

--- Class Questions/cmath, math.py
+++ Class Questions/cmath, math.py
@@ -10,16 +10,23 @@ x = float(input('Enter X(First real Number) : '))
 y = float(input('Enter X(Second real Number) : '))
 l = int(input('Enter Base : '))
 
+# converting x and y into complex number
 z = complex(x,y);
 
+# converting complex number into polar using polar()
 w = cmath.polar(z)
 
+#Making List
 my_list = list(w)
 
+# converting complex number into rectangular using rect()
 w = cmath.rect(my_list[0], my_list[1])
 
-base_log = cmath.log(z,10)
+#log() returns natural logarithm of x, for x > 0.
+base_log = cmath.log(z,l)
 
+
+#printing statements
 print ("The modulus and argument of polar complex number is : ",w)
 
 print ("The rectangular form of complex number is : ",end="")
